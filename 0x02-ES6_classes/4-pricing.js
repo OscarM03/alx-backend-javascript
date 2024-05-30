0f5b25564a9ff7b4636d4ab078861d5e2c33e089
OscarM03 <oscarmutwiri03@gmail.com>
2024-05-30 15:09:44 +0300

    Implementing class Pricing that returns the full price

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // setter
+  set amount(num) {
+    if (typeof num !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = num;
+  }
+
+  // getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // setter
+  set currecny(symbol) {
+    if (!(this.currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = symbol;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+}
