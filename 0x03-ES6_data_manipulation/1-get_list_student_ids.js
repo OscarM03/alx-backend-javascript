82d34a96a2ce17b3923f478e065d37ab26d499f8
OscarM03 <oscarmutwiri03@gmail.com>
2024-06-03 12:24:43 +0300

    A function that returns the ids of students from an object using map

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,8 +2,5 @@ export default function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
-  const ids = [];
-  students.map((std) => ids.push(std.id));
-
-  return ids;
+  return students.map((std) => std.id);
 }
