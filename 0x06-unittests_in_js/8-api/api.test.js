a4d5f514b6b447a2d10f4e01148f83234a61526c
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-25 17:39:57 +0300

    Adding basic integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./api');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('index Page', () => {
+  it('should return status code 200', () => new Promise((done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  }));
+  it('should return the correct message', () => new Promise((done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.text).to.equal('Welcome to the payment system');
+        done();
+      });
+  }));
+});
